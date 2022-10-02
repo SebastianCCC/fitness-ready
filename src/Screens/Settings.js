546199330dcea9bff8d33fc89fb70a96563de2ae
SebastianCCC <89015968+SebastianCCC@users.noreply.github.com>
2022-10-02 19:05:41 +0200

    add the ability to change username

--- src/Screens/Settings.js
+++ src/Screens/Settings.js
@@ -3,20 +3,22 @@ import {
   View,
   Text,
   TouchableHighlight,
+  TouchableWithoutFeedback,
   KeyboardAvoidingView,
   TextInput,
+  Keyboard,
 } from 'react-native'
 import { tw } from '../../tailwind'
 import { useContext, useEffect, useState } from 'react'
 import { StateContext } from '../Util/StateContext'
-import { signOut, deleteUser } from 'firebase/auth'
+import { signOut, updateProfile } from 'firebase/auth'
 import { CommonActions } from '@react-navigation/native'
 import { auth } from '../../firebase'
 import NavigationHeader from '../Components/Header/NavigationHeader'
 import { LinearGradient } from 'expo-linear-gradient'
 
 export default function Settings({ navigation }) {
-  const { user } = useContext(StateContext)
+  const { user, setUser } = useContext(StateContext)
   const [username, setUsername] = useState(user.displayName)
 
   const SignUserOut = () => {
@@ -35,16 +37,13 @@ export default function Settings({ navigation }) {
       })
   }
 
-  const DeleteAccount = () => {
-    deleteUser(auth.currentUser)
+  const UpdateUser = () => {
+    updateProfile(auth.currentUser, {
+      displayName: username,
+    })
       .then(() => {
-        navigation.navigate('Welcome')
-        navigation.dispatch(
-          CommonActions.reset({
-            index: 1,
-            routes: [{ name: 'Welcome' }],
-          })
-        )
+        setUser({ ...user, displayName: username })
+        Keyboard.dismiss()
       })
       .catch((error) => {
         console.log(error.message)
@@ -53,70 +52,74 @@ export default function Settings({ navigation }) {
 
   return (
     <>
-      <NavigationHeader navigation={navigation} />
-      <View style={tw`flex-1 bg-primary`}>
-        <Text style={tw`text-white uppercase text-sm font-bold p-page`}>Account Information</Text>
-        <KeyboardAvoidingView behavior="padding">
-          <LinearGradient
-            start={{ x: 0, y: 0 }}
-            end={{ x: 1, y: 0 }}
-            colors={['#4630EB', '#6453E2']}
-            style={tw`px-page`}
-          >
-            <View style={tw`flex flex-row justify-between items-center`}>
-              <Text style={tw`text-tertiary text-base uppercase font-bold italic`}>Username</Text>
-              <TextInput
-                onChangeText={(text) => setUsername(text)}
-                value={username ? username : null}
-                placeholder={!username ? 'Change Username' : null}
-                placeholderTextColor="#BCC3CD"
-                autoFocus={false}
-                style={tw`w-[75%] text-tertiary text-right font-bold italic py-space`}
-              />
+      <NavigationHeader
+        navigation={navigation}
+        userName={user.displayName}
+        currentUsername={username}
+        save={UpdateUser}
+      />
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+        <View style={tw`flex-1 bg-primary p-page`}>
+          <Text style={tw`text-white uppercase text-sm font-bold pb-page`}>
+            Account Information
+          </Text>
+          <KeyboardAvoidingView behavior="padding">
+            <LinearGradient
+              start={{ x: 0, y: 0 }}
+              end={{ x: 1, y: 0 }}
+              colors={['#4630EB', '#6453E2']}
+              style={tw`px-page rounded-md`}
+            >
+              <View style={tw`flex flex-row justify-between items-center`}>
+                <Text style={tw`text-tertiary text-base uppercase font-bold italic`}>Username</Text>
+                <TextInput
+                  onChangeText={(text) => setUsername(text)}
+                  value={username ? username : null}
+                  placeholder={!username ? 'Change Username' : null}
+                  placeholderTextColor="#BCC3CD"
+                  autoCorrect={false}
+                  autoFocus={false}
+                  style={tw`w-[75%] text-tertiary text-right font-bold italic py-space`}
+                />
+              </View>
+            </LinearGradient>
+            <View
+              style={tw`bg-secondary/50 px-page py-space mt-[10px] rounded-md flex-row justify-between items-center`}
+            >
+              <Text style={tw`text-tertiary text-base uppercase font-bold italic`}>Email</Text>
+              <Text style={tw`text-tertiary italic`}>{user.email}</Text>
             </View>
-          </LinearGradient>
-          <View
-            style={tw`bg-secondary/50 px-page py-space mt-[10px] flex-row justify-between items-center`}
-          >
-            <Text style={tw`text-tertiary text-base uppercase font-bold italic`}>Email</Text>
-            <Text style={tw`text-tertiary italic`}>{user.email}</Text>
+          </KeyboardAvoidingView>
+          <View style={tw`mt-[30px]`}>
+            <View
+              style={tw`bg-secondary/50 px-page py-space rounded-md flex-row justify-between items-center`}
+            >
+              <Text style={tw`text-tertiary text-base uppercase font-bold italic`}>User Id</Text>
+              <Text style={tw`text-tertiary italic`}>{user.uid}</Text>
+            </View>
+            <View
+              style={tw`bg-secondary/50 px-page py-space mt-[10px] rounded-md flex-row justify-between items-center`}
+            >
+              <Text style={tw`text-tertiary text-base uppercase font-bold italic`}>Anonymous</Text>
+              <Text style={tw`text-tertiary italic`}>{user.isAnonymous.toString()}</Text>
+            </View>
+            <View
+              style={tw`bg-secondary/50 px-page py-space mt-[10px] rounded-md flex-row justify-between items-center`}
+            >
+              <Text style={tw`text-tertiary text-base uppercase font-bold italic`}>Os</Text>
+              <Text style={tw`text-tertiary italic`}>{Platform.Version}</Text>
+            </View>
           </View>
-        </KeyboardAvoidingView>
-        <View style={tw`mt-[30px]`}>
-          <View style={tw`bg-secondary/50 px-page py-space flex-row justify-between items-center`}>
-            <Text style={tw`text-tertiary text-base uppercase font-bold italic`}>User Id</Text>
-            <Text style={tw`text-tertiary italic`}>{user.uid}</Text>
-          </View>
-          <View
-            style={tw`bg-secondary/50 px-page py-space mt-[10px] flex-row justify-between items-center`}
-          >
-            <Text style={tw`text-tertiary text-base uppercase font-bold italic`}>Anonymous</Text>
-            <Text style={tw`text-tertiary italic`}>{user.isAnonymous.toString()}</Text>
-          </View>
-          <View
-            style={tw`bg-secondary/50 px-page py-space mt-[10px] flex-row justify-between items-center`}
-          >
-            <Text style={tw`text-tertiary text-base uppercase font-bold italic`}>Os</Text>
-            <Text style={tw`text-tertiary italic`}>{Platform.Version}</Text>
+          <View style={tw`flex-1 justify-end mb-[15px]`}>
+            <TouchableHighlight
+              onPress={SignUserOut}
+              style={tw`bg-white p-page rounded-md mt-[30px]`}
+            >
+              <Text style={tw`uppercase font-bold italic text-center`}>Log out</Text>
+            </TouchableHighlight>
           </View>
         </View>
-        <View style={tw`flex-1 justify-end mb-[30px]`}>
-          <TouchableHighlight
-            onPress={SignUserOut}
-            style={tw`bg-warning px-page py-space mt-[30px]`}
-          >
-            <Text style={tw`text-white uppercase font-bold italic text-center`}>Log out</Text>
-          </TouchableHighlight>
-          <TouchableHighlight
-            onPress={DeleteAccount}
-            style={tw`bg-secondary px-page py-space mt-[10px]`}
-          >
-            <Text style={tw`text-white uppercase font-bold italic text-center`}>
-              delete account
-            </Text>
-          </TouchableHighlight>
-        </View>
-      </View>
+      </TouchableWithoutFeedback>
     </>
   )
 }
